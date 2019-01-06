6c3c486b9795477355b6ba8e743d6e26c9e2121a
TarasKendzor <45481754+TarasKendzor@users.noreply.github.com>
2019-01-06 15:07:08 +0200

    done

--- script/index.js
+++ script/index.js
@@ -1,12 +1,15 @@
   $(document).ready(function () {
 
-    $(".scroll-down").on("click", function () {
+    $('.scroll-down').on('click', function () {
         $('html, body').animate({
-            scrollTop: $(".main").offset().top
+            scrollTop: $('.main').offset().top
         }, 1000);
     });
 
-    
+    $('#play-button').hover(function(){
+      $('.hover-video').fadeOut(500);
+
+    });
 
 
 
